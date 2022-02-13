cb04b615333776482a7dda29c384c02e99dedc72
samyon7 <sammyon7@gmail.com>
2022-02-13 13:23:09 +0700

    Learn Array

---
+++ Work_2/array.php
@@ -0,0 +1,29 @@
+<?php
+$heroInfo = [
+[
+'heroName' => 'Spiderman',
+'weapon' => 'Spider web'
+],
+[
+'heroName' => 'Iron Man',
+'weapon' => 'Mark L'
+],
+[
+'heroName' => 'Thor',
+'weapon' => 'Mjolnir'
+],
+[
+'heroName' => 'Captain America',
+'weapon' => 'Shield'
+]
+];
+
+echo '<pre>';
+print_r($heroInfo);
+echo '<pre>';
+
+echo 'The weapon of choice for '.$heroInfo[3]['heroName'].' is '.$heroInfo[3]['weapon'];
+echo '<br>';
+
+
+?>
