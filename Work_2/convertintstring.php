1763e509fc10dea1655e4906f8d58fb33d69880e
samyon7 <sammyon7@gmail.com>
2022-02-14 11:02:31 +0700

    Integer to String

---
+++ Work_2/convertintstring.php
@@ -0,0 +1,14 @@
+<?php
+
+echo '<h3>Int to String</h3>';
+$number = 1234;
+echo '<h3>Before type conversion:</h3>';
+var_dump($number);
+
+echo '<h3>After type conversion:</h3>';
+
+$stringValue = (string)($number);
+
+var_dump($stringValue);
+
+?>
