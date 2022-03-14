3121f0e7e77b657aa5b54089a28647b2826775ab
samyon7 <sammyon7@gmail.com>
2022-03-14 11:41:30 +0700

    Abstracting class

--- Work_5/Motorcycle.php
+++ Work_5/Motorcycle.php
@@ -1,15 +1,26 @@
 <?php
 
-require_once'Vehicle.php';
+require_once'AbstractVehicle.php';
 
-class Motorcycle extends Vehicle{
+class Motorcycle extends AbstractVehicle{
 	public $noOfWheels = 2;
 	public $stroke = 4;
+
+	private $hasKey = true;
+	private $hasKicked = true;
+
+	public function start(){
+		if($this->hasKey && $this->hasKicked){
+			$this->engineStatus = true;
+		}
+	}
 }
 
-$motorcycle1 = new Motorcycle('Kawasaki', 'Ninja', 'Orange', 2, '53WVC14598');
-$motorcycle2 = new Motorcycle('Suzuki', 'Gixxer SF', 'Blue', 2, '53WVC14599');
-$motorcycle2 = new Motorcycle('Harley Davidson', 'Street 750', 'Black', 2, '53WVC14234');
-echo "Available motorcycles are " . Motorcycle::$counter. PHP_EOL;
+$motorcycle = new Motorcycle('Kawasaki', 'Ninja', 'Orange', 2, '53WVC14598');
+$motorcycle->start();
+echo "The motorcycle is " . ($motorcycle->getEngineStatus()?'running': 'stopped') . PHP_EOL;
+$motorcycle->stop();
+echo "The motorcycle is " . ($motorcycle->getEngineStatus()?'running': 'stopped') . PHP_EOL;
+
 
 ?>
